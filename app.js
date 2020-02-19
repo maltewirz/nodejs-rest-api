ae842dee1ef7eecfd92272dd7457ec8becdff6fd
Malte Wirz <mail@maltewirz.com>
2020-02-19 07:43:15 +0100

    mongoose connected

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const secrets = require('./util/secrets');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
 
 app.use(express.json());
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -15,5 +19,17 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    res.status(status).json({ message: message });
+});
+
+const MONGODB_URI = `mongodb+srv://${secrets.mongoDbUser}:${secrets.mongoDbPassword}@cluster0-f8kmd.gcp.mongodb.net/messages`;
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true  })
+    .then(() => {
+        app.listen(8080);
+    })
+    .catch(err => console.log(err));
 
