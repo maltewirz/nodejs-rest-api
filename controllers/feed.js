1b0c51e3f942385b6e96f754722efc6d705c9eaf
Malte Wirz <mail@maltewirz.com>
2020-02-19 06:59:44 +0100

    working MVP

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,6 +1,15 @@
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
-        posts: [{ title: 'First Post', content: 'This is the first post'}]
+        posts: [{
+            _id: '1',
+            title: 'First Post', 
+            content: 'This is the first post',
+            imageUrl: 'images/duck.jpeg',
+            creator: {
+                name: 'Dude'
+            },
+            createdAt: new Date()
+        }]
     });   
 };
 
@@ -11,9 +20,11 @@ exports.createPost = (req, res, next) => {
     res.status(201).json({
         message: 'Post created successfully',
         post: {
-            id : new Date().toISOString(),
+            _id : new Date().toISOString(),
             title: title,
-            content: content
+            content: content,
+            creator: { name: 'Dude'},
+            createdAt: new Date()
         }
     });
 };
